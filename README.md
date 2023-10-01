225183fa0694895bc495faa1a2778fabc0c32089
Maxwell Kimaiyo <maxwellkimaiyo@gmail.com>
2023-10-01 15:12:16 +0300

    ⚒️build: Set up docker compose with postgres database

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # loan-projections-service
+mvn spring-boot:run -Pdocker-compose -Dspring-boot.run.profiles=docker-compose
--- src/main/java/com/jia/loanprojectionsservice/LoanProjectionsServiceApplication.java
+++ src/main/java/com/jia/loanprojectionsservice/LoanProjectionsServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoanProjectionsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoanProjectionsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoanProjectionsServiceApplication.class, args);
+    }
 
 }
