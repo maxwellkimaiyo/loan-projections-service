174572fa70635e9ba726973440b1a7d0ac961ea3
Maxwell Kimaiyo <maxwellkimaiyo@gmail.com>
2023-10-01 11:20:01 +0300

    🎉initial: Service create
    Signed-off-by: Maxwell Kimaiyo <maxwellkimaiyo@gmail.com>

---
+++ src/main/java/com/jia/loanprojectionsservice/LoanProjectionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jia.loanprojectionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanProjectionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanProjectionsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jia/loanprojectionsservice/LoanProjectionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jia.loanprojectionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanProjectionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
